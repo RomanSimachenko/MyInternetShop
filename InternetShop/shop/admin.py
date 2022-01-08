96d8eee3fdd99f0b7584d0ab26104dc8592cccfb
r0m1mPL <simachenko.roma@gmail.com>
2022-01-09 00:03:07 +0200

    update

--- InternetShop/shop/admin.py
+++ InternetShop/shop/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import models
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Товары"""
+    list_display = ('name', 'category', 'price', 'created',)
--- InternetShop/shop/models.py
+++ InternetShop/shop/models.py
@@ -1,1 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import SET_NULL
+
+
+class Category(models.Model):
+    """Категории"""
+    name = models.CharField("Категория", max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    """Товары"""
+    name = models.CharField("Товар", max_length=300)
+    category = models.ForeignKey(
+        Category, verbose_name="Категория", on_delete=SET_NULL, null=True)
+    description = models.TextField("Описание")
+    price = models.PositiveIntegerField(
+        "Цена", default=0, help_text="указывать сумму в гривнах")
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
