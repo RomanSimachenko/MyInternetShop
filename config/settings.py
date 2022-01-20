b0e9481bfb18c913e9a254de926db341c1480ea1
r0m1mPL <simachenko.roma@gmail.com>
2022-01-20 23:45:55 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = bool(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = ['*']
 
